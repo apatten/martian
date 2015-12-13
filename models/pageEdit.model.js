aa68da872e5708e9ddf74aa44048d5ee640a0bba
Karen Ananiev <karen.ananiev@gmail.com>
2015-12-13 22:46:05 +0300

    added pageBase and pageOverwritten to the page edit model

--- models/pageEdit.model.js
+++ models/pageEdit.model.js
@@ -7,6 +7,12 @@ let pageEditModel = {
             status: obj['@status'],
             page: pageModel.parse(obj.page)
         };
+        if('page.base' in obj) {
+            parsed.pageBase = pageModel.parse(obj['page.base']);
+        }
+        if('page.overwritten' in obj) {
+            parsed.pageOverwritten = pageModel.parse(obj['page.overwritten']);
+        }
         return parsed;
     }
 };
