c0fd54cb429478e8eb3d930d1fb140ae950efe41
JeremyRH <holcomb227@gmail.com>
2016-10-28 10:53:29 -0700

    use correct semantic versioning

--- models/pageHistory.model.js
+++ models/pageHistory.model.js
@@ -18,45 +18,18 @@
  */
 import { eventModel } from './event.model.js';
 export const pageHistoryModel = [
-    {
-        field: '@count',
-        name: 'count',
-        transform: 'number'
-    },
-    {
-        field: '@upto',
-        name: 'upto'
-    },
-    {
-        field: '@since',
-        name: 'since'
-    },
+    { field: '@count', name: 'count', transform: 'number' },
+    { field: '@upto', name: 'upto' },
+    { field: '@since', name: 'since' },
     {
         field: 'summary',
         isArray: true,
         transform: [
-            {
-                field: '@id',
-                name: 'id'
-            },
-            {
-                field: '@datetime',
-                name: 'datetime',
-                transform: 'date'
-            },
-            {
-                field: '@count',
-                name: 'count',
-                transform: 'number'
-            },
-            {
-                field: '@uri.detail',
-                name: 'uriDetail'
-            },
-            {
-                field: 'event',
-                transform: eventModel
-            }
+            { field: '@id', name: 'id' },
+            { field: '@datetime', name: 'datetime', transform: 'date' },
+            { field: '@count', name: 'count', transform: 'number' },
+            { field: '@uri.detail', name: 'uriDetail' },
+            { field: 'event', transform: eventModel }
         ]
     }
 ];
