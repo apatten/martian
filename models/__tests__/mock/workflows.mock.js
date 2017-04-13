bd481d86e1617c93910e0f73eab0f077720a01a1
jeremyrh <holcomb227@gmail.com>
2017-04-13 15:31:21 -0700

    remove old mocks and tests

--- models/__tests__/mock/workflows.mock.js
+++
@@ -1,6 +0,0 @@
-export const workflowsMock = {
-    workflows: {
-        "@href": "https://responsive.mindtouch.us/@api/deki/workflow/submit-issue",
-        "uri.next": "https://responsive.mindtouch.us/Contact_Support/Request_Article"
-    }
-};
