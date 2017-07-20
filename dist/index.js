e2f6828c0d0a5f50cc141be0808da809e64fd09a
Andy Vaughn <andyv@mindtouch.com>
2017-07-20 16:13:53 -0700

    mend

--- dist/index.js
+++ dist/index.js
@@ -7423,8 +7423,6 @@ class WorkflowManager {
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @preserve
  */
 
 exports.Settings = Settings;
