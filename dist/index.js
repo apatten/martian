b13bcc4cdbd001382a51650fb0b8187af3a5ab4a
Aaron <mars.aaron@gmail.com>
2017-09-06 14:09:11 -0700

    Prepare for release 1.31.0

--- dist/index.js
+++ dist/index.js
@@ -1687,7 +1687,10 @@ class DeveloperTokenManager {
             requestXml += `<host>${host}</host>`;
         }
         requestXml += '</developer-token>';
-        return this._plug.post(requestXml, utility.xmlRequestType).then((r) => r.json()).then(modelParser.createParser(developerTokenModel));
+        return this._plug.post(requestXml, utility.xmlRequestType)
+            .then((r) => r.json())
+            .catch((err) => Promise.reject(_errorParser(err)))
+            .then(modelParser.createParser(developerTokenModel));
     }
 }
 
@@ -5188,9 +5191,12 @@ class Site {
      * @param {String} [options.bucket] - The time you want to bucket results into
      * @param {String} [options.origin] - The source of the search query (mt-web, mt-api, etc)
      * @param {String} [options.webWidgetEmbedId] - the embed id for the source web widget
+     * @param {String} [options.sortBy] - Sort table data by this field (e.g. clicks, position) (default: clicks)
+     * @param {String} [options.sortOrder] - Sort direction to be used with sortby (e.g. asc, desc) (default: desc)
+     * @param {Number} [options.limit] - Number of clicked results to return results for (between 1 and 1000 inclusive) (default: 100)
      * @returns {Promise.<Object>} - A Promise that will be resolved with the search analytics data, or rejected with an error specifiying the reason for rejection.
      */
-    getSearchAnalytics({ start = null, end = null, queryFilters = null, userFilter = null, bucket = null, origin = null, webWidgetEmbedId = null }) {
+    getSearchAnalytics({ start = null, end = null, queryFilters = null, userFilter = null, bucket = null, origin = null, webWidgetEmbedId = null, sortBy = null, sortOrder = null, limit = null }) {
         const searchParams = {
             start,
             end,
@@ -5198,7 +5204,10 @@ class Site {
             userFilter,
             bucket,
             originFilter: origin,
-            web_widget_embed_id: webWidgetEmbedId // eslint-disable-line camelcase
+            web_widget_embed_id: webWidgetEmbedId, // eslint-disable-line camelcase
+            sortby: sortBy,
+            sortorder: sortOrder,
+            limit
         };
         return this.plug.at('search', 'analytics').withParams(utility.cleanParams(searchParams)).get().then((r) => r.json()).then(modelParser.createParser(searchAnalyticsModel));
     }
@@ -5218,7 +5227,7 @@ class Site {
      * @param {Number} [options.limit] - Number of clicked results to return results for (between 1 and 1000 inclusive) (default: 100)
      * @returns {Promise.<Object>} - A Promise that will be resolved with the search analytics data, or rejected with an error specifiying the reason for rejection.
      */
-    getSearchAnalyticsQuery({ query, start = null, end = null, queryFilters = null, userFilter = null, bucket = null, origin = null, webWidgetEmbedId = null, sortBy = null, sortOrder = null, limit = null }) {
+    getSearchAnalyticsQuery({ query, start = null, end = null, userFilter = null, bucket = null, origin = null, webWidgetEmbedId = null, sortBy = null, sortOrder = null, limit = null }) {
         const searchParams = {
             query,
             start,
@@ -5226,7 +5235,7 @@ class Site {
             userFilter,
             bucket,
             originFilter: origin,
-            web_widget_embed_id: webWidgetEmbedId,
+            web_widget_embed_id: webWidgetEmbedId, // eslint-disable-line camelcase
             sortby: sortBy,
             sortorder: sortOrder,
             limit
