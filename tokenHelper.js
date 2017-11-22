0583bbf29602f6003cbc5e18c9fd870961601a75
JeremyRH <holcomb227@gmail.com>
2017-11-22 12:54:34 -0800

    use single function for token generation

--- tokenHelper.js
+++ tokenHelper.js
@@ -1,17 +1,13 @@
 /* eslint-env node */
 const crypto = require('crypto');
 
-function getToken(key, secret, user) {
-    const hmac = crypto.createHmac('sha256', secret);
-    const epoch = Math.floor(Date.now() / 1000);
-    hmac.update(`${key}${epoch}${user}`);
-    const hash = hmac.digest('hex');
-    return `int_${key}_${epoch}_${hash}`;
-}
-
-exports.nameTokenGenerator = (key, secret) => {
-    return (username) => getToken(key, secret, `=${username}`);
-};
-exports.idTokenGenerator = (key, secret) => {
-    return (userId) => getToken(key, secret, userId);
+exports.tokenGenerator = (key, secret) => {
+    return (user) => {
+        user = typeof user === 'number' ? user : `=${user}`;
+        const hmac = crypto.createHmac('sha256', secret);
+        const epoch = Math.floor(Date.now() / 1000);
+        hmac.update(`${key}${epoch}${user}`);
+        const hash = hmac.digest('hex');
+        return `int_${key}_${epoch}_${hash}`;
+    };
 };
