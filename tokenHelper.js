e652d112c47af4f501889ac2295a629a13878564
JeremyRH <holcomb227@gmail.com>
2017-11-22 11:22:05 -0800

    update tokenHelper

--- tokenHelper.js
+++ tokenHelper.js
@@ -1,12 +1,17 @@
 /* eslint-env node */
 const crypto = require('crypto');
 
-exports.createHelper = function(key, secret) {
-    return () => {
-        let hmac = crypto.createHmac('sha256', secret);
-        let epoch = Math.floor(Date.now() / 1000);
-        hmac.update(`${key}${epoch}`);
-        let hash = hmac.digest('hex');
-        return `${key}_${epoch}_${hash}`;
-    };
+function getToken(key, secret, user) {
+    const hmac = crypto.createHmac('sha256', secret);
+    const epoch = Math.floor(Date.now() / 1000);
+    hmac.update(`${key}${epoch}${user}`);
+    const hash = hmac.digest('hex');
+    return `int_${key}_${epoch}_${hash}`;
+}
+
+exports.nameTokenGenerator = (key, secret) => {
+    return (username) => getToken(key, secret, `=${username}`);
+};
+exports.idTokenGenerator = (key, secret) => {
+    return (userId) => getToken(key, secret, userId);
 };
