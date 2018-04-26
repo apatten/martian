af4f3c90d1ba2b695bfb67570ad96037979b27b2
Benjamin Herron <bmherron@Benjamins-MacBook-Pro.local>
2018-04-26 14:41:21 -0700

    added apiErrorModel import to pageKcs.js

--- pageKcs.js
+++ pageKcs.js
@@ -4,6 +4,9 @@ import { modelParser } from './lib/modelParser.js';
 import { utility } from './lib/utility.js';
 import { kcsTransitionsModel } from './models/kcsTransitions.model.js';
 import { kcsStateModel } from './models/kcsState.model.js';
+import { apiErrorModel } from './models/apiError.model.js';
+
+const _errorParser = modelParser.createParser(apiErrorModel);
 
 /**
  * A class for handling KCS actions
