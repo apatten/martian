e3cb15901fc8df48a20234bc455da769bf35c245
Aaron Mars <mars.aaron@gmail.com>
2019-01-28 09:23:31 -0800

    Keep automock off

--- .jest-config-common.js
+++ .jest-config-common.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 module.exports = {
-    automock: false,
     collectCoverage: true,
     collectCoverageFrom: ['<rootDir>/*.js', '<rootDir>/lib/*.js'],
     coverageDirectory: '__coverage-node__',
